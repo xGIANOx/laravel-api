7ef6f4b2d511d2a79e3d7bbcff77450d7c5efb79
xGIANOx <studioenricogian@gmail.com>
2023-06-07 16:57:49 +0200

    [fix] header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{route('admin.projects.index')}}">{{ __('Admin') }}</a>
+                            <a class="nav-link" href="{{route('admin.projects.index')}}">{{ __('Projects') }}</a>
                         </li>
                     </ul>
 
