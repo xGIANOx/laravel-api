e3d2ee6e9b171434600544225f304255bcd008c1
xGIANOx <studioenricogian@gmail.com>
2023-06-08 17:02:53 +0200

    prova repo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
+                            <a class="nav-link" href="{{ route('admin.projects.index') }}">{{ __('Admin') }}</a>
                         </li>
                     </ul>
 
