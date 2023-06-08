4dbf2a46c1b59494636dd1533205be9d05991fec
xGIANOx <studioenricogian@gmail.com>
2023-06-08 17:27:08 +0200

    [add] belongs to many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -14,6 +15,11 @@ class Project extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 
 }
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Technology extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
